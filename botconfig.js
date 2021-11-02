f56defe7ceeacb46b8e34f09a46eda9921a8c14e
kabuspl <59837830+kabuspl@users.noreply.github.com>
2021-11-02 20:33:31 +0100

    lavalink replit just stopped working...

--- botconfig.js
+++ botconfig.js
@@ -28,7 +28,7 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "lavalink-replit.jakubsakra.repl.co",
+    host: "lavalink-replit-1.jakubsakra.repl.co",
     port: 443, // The port that lavalink is listening to. This must be a number!
     pass: "maybeiwasboring",
     secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
